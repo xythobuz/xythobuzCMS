8b0d11fa8e90057e597db9b1117bb1f202bd4536
Thomas Buck <taucher.bodensee@googlemail.com>
2012-07-14 18:51:52 +0200

    Can now render different numbers of days into the past.

--- render.php
+++ render.php
@@ -1,13 +1,14 @@
 <?
-	// Do not call this directly!
+	// Do not call this directly! We need a opened DB connection.
 	if (basename($_SERVER['PHP_SELF']) == "render.php") {
 		include("config.php");
-		header('Location: '.$xythobuzCMS_root.'/stats.php?img');
+		header('Location: '.$xythobuzCMS_root.'/stats.php');
 		exit;
 	}
 
 	// We create a diagram of the monthly visitors
 	header("Content-type: image/png");
+	// Custom or preset width / height
 	if ((isset($_GET['w'])) && (isset($_GET['h']))) {
 		define("WIDTH", $_GET['w']);
 		define("HEIGHT", $_GET['h']);
@@ -31,6 +32,7 @@
 		define("RADIUS", 8);
 	}
 
+	// Used colors
 	$img = @imagecreate(WIDTH, HEIGHT)
 			or die ("Can't create GD Stream!");
 	$white = imagecolorallocate($img, 255, 255, 255); // Set white background
@@ -38,6 +40,71 @@
 	$grey = imagecolorallocate($img, 176, 176, 69);
 	$blue = imagecolorallocate($img, 42, 42, 242);
 
+	if (isset($_GET['d'])) { // d is the number of days to show, ending today
+		$d = $_GET['d'];
+		if (date('d') < $d) { // Can't go past current month
+			$d = date('d');
+		}
+		$start = (date('d') - $d);
+		$printDays = 1; // Print no days, cause it's fucked up
+		$clause = 'day > SUBDATE(NOW(), '.$d.')';
+	} else {
+		$d = 0;
+		$start = 1;
+		$clause = 'MONTH(day) = '.date('m');
+		$printDays = 0; // Print days on y-axis
+	}
+	$day = date('d');
+
+	if ($renderVisitors) {
+		$max = maxThisMonthB($d);
+	} else if ($renderBots) {
+		$max = maxThisMonthC($d);
+	} else {
+		$max = maxThisMonth($d);
+	}
+
+	// Render datapoints
+	if ($renderVisitors) {
+		$sql = 'SELECT count(ip) AS visitors, day
+		FROM cms_visit
+		GROUP BY day
+		HAVING '.$clause.'
+		ORDER BY day ASC';
+	} else if ($renderBots) {
+		$sql = 'SELECT day, bots AS visitors
+		FROM cms_bots
+		WHERE '.$clause.'
+		ORDER BY day ASC';
+	} else {
+		$sql = 'SELECT day, visitors
+		FROM cms_visitors
+		WHERE '.$clause.'
+		ORDER BY day ASC';
+	}
+
+	$result = mysql_query($sql);
+	if (!$result) {
+		// Show error, exit
+		imagestring($img, 5, (WIDTH / 2), (HEIGHT / 2), "Database Error!", $blue);
+		imagestring($img, 2, (WIDTH - 145), (HEIGHT - 20), "rendered by xythobuzCMS", $grey);
+		imagepng($img);
+		exit;
+	}
+
+	// Display data points
+	$oldPoint = 0;
+
+	while ($row = mysql_fetch_array($result)) {
+		$date = str_replace(date('Y-m-'), "", $row['day']); // Strip month and year
+		if ($oldPoint == 0) {
+			$oldPoint = drawPoint($date, $row['visitors'], $max, $day, $img, $blue, $black, NULL, $printDays, $start);
+		} else {
+			$oldPoint = drawPoint($date, $row['visitors'], $max, $day, $img, $blue, $black, $oldPoint, $printDays, $start);
+		}
+	}
+
+	// Draw the usual stuff
 	imagefilledrectangle($img, 0, 0, WIDTH-1, 1, $black); // Line top
 	imagefilledrectangle($img, 0, 0, 1, HEIGHT-1, $black); // Line left
 	imagefilledrectangle($img, WIDTH-1, 0, WIDTH-2, HEIGHT-1, $black); // Line right
@@ -49,17 +116,8 @@
 	} else {
 		imagestring($img, 4, 3, 3, "Pageviews ".date('m.Y')." ".$xythobuzCMS_title, $black);
 	}
-
 	imagefilledrectangle($img, 50, 30, 52, (HEIGHT - 15), $black); // Y-Axis
 	imagefilledrectangle($img, 40, (HEIGHT - 25), (WIDTH - 15), (HEIGHT - 27), $black); // X-Axis
-
-	if ($renderVisitors) {
-		$max = maxThisMonthB();
-	} else if ($renderBots) {
-		$max = maxThisMonthC();
-	} else {
-		$max = maxThisMonth();
-	}
 
 	imagefilledrectangle($img, 42, 50, 60, 52, $black); // Max mark on y axis
 	imagefilledrectangle($img, 42, ((diff(YSTART, YEND) / 2) + YEND - 2),
@@ -105,8 +163,6 @@
 		imagestring($img, 3, 5, ((diff(YSTART, YEND) / 4) + YEND - 8), $str, $black); // 3 Quarter number
 	}
 
-	$day = date('d'); // For maximum x value
-
 	if (WIDTH >= 200) {
 		// Large enough that we want to render arrows at the axis tops
 		imageline($img, 51, 30, 41, 40, $black); // Y-Axis, to the left
@@ -114,47 +170,7 @@
 		imageline($img, (WIDTH - 15), (HEIGHT - 26), (WIDTH - 20), (HEIGHT - 34), $black); // X-Axis, to the top
 		imageline($img, (WIDTH - 15), (HEIGHT - 26), (WIDTH - 20), (HEIGHT - 18), $black); // X-Axis, to the bottom
 	}
-
-	// Render datapoints
-	if ($renderVisitors) {
-		$sql = 'SELECT count(ip) AS visitors, day
-		FROM cms_visit
-		GROUP BY day
-		HAVING MONTH(day) = '.date('m').'
-		ORDER BY day ASC';
-	} else if ($renderBots) {
-		$sql = 'SELECT day, bots AS visitors
-		FROM cms_bots
-		WHERE MONTH(day) = '.date('m').'
-		ORDER BY day ASC';
-	} else {
-		$sql = 'SELECT day, visitors
-		FROM cms_visitors
-		WHERE MONTH(day) = '.date('m').'
-		ORDER BY day ASC';
-	}
-
-	$result = mysql_query($sql);
-	if (!$result) {
-		// Show error, exit
-		imagestring($img, 5, (WIDTH / 2), (HEIGHT / 2), "Database Error!", $blue);
-		imagestring($img, 2, (WIDTH - 145), (HEIGHT - 20), "rendered by xythobuzCMS", $grey);
-		imagepng($img);
-		exit;
-	}
-
-	// Display data points
-	$oldPoint = 0;
-	while ($row = mysql_fetch_array($result)) {
-		$date = str_replace(date('Y-m-'), "", $row['day']); // Strip month and year
-		if ($oldPoint == 0) {
-			$oldPoint = drawPoint($date, $row['visitors'], $max, $day, $img, $blue, $black);
-		} else {
-			$oldPoint = drawPoint($date, $row['visitors'], $max, $day, $img, $blue, $black, $oldPoint);
-		}
-	}
-
-	// Finish
+	
 	imagestring($img, 2, 4, 17, "rendered by xythobuzCMS", $grey);
 	imagepng($img);
 
@@ -165,24 +181,27 @@
 	// ------------------------------------
 
 	// x = days, from 1 to $maxDay
-	// y = views, from 0 to $max
-	function drawPoint($x, $y, $maxViews, $maxDay, $img, $color1, $color2, $oldPoint) {
+	// y = views, from 0 to $maxViews
+	// printDays: 0 if you want days to be printed
+	function drawPoint($x, $y, $maxViews, $maxDay, $img, $color1, $color2, $oldPoint = NULL, $printDays = 0, $minDay = 1) {
 		// Beauty fix for first day of month
-		if ($maxDay == 1) {
+		if ($maxDay <= 1) {
 			$maxDay = 2;
 		}
 
 		// Draw point
-		$a = ((diff(XSTART, XEND) * $x / $maxDay) + XSTART); // X Point
+		$minDay = $minDay - 1;
+		$a = ((diff(XSTART, XEND) * ($x - $minDay) / ($maxDay - $minDay + 1)) + XSTART); // X Point
 		$b = ((diff(YSTART, YEND) * ($maxViews - $y) / $maxViews) + YEND); // Y Point
 		imagefilledellipse($img, $a, $b, RADIUS, RADIUS, $color1); // Point
 		imagefilledrectangle($img, $a-1, HEIGHT-20, $a, HEIGHT-32, $color2); // Line on x-Axis
-		if (($x == $maxDay) || ($x == ($maxDay / 2))) {
+		
+		if (($printDays == 0) && ((($x - $minDay) == $maxDay) || (($x - $minDay) == ($maxDay / 2)))) {
 			imagestring($img, 3, $a-7, HEIGHT-17, $x, $color2); // Day
 		}
 
 		// Draw line to last point
-		if (isset($oldPoint)) {
+		if (isset($oldPoint) && (!is_null($oldPoint) && ($x != 1))) {
 			imageline($img, $oldPoint[0], $oldPoint[1], $a, $b, $color1);
 		}
 
@@ -193,9 +212,15 @@
 		return $ret;
 	}
 
-	// The highest daily pageviews this month
-	function maxThisMonth() {
-		$sql = 'SELECT day, visitors FROM cms_visitors WHERE MONTH(day) = '.date('m');
+
+	// The highest daily pageviews this month, or last $d days
+	function maxThisMonth($d = 0) {
+		$sql = 'SELECT day, visitors FROM cms_visitors WHERE ';
+		if ($d == 0) {
+			$sql .= 'MONTH(day) = '.date('m');
+		} else {
+			$sql .= 'day > SUBDATE(NOW(), '.$d.')';
+		}
 		$result = mysql_query($sql);
 		if ($result) {
 			$maxVisitors = 0;
@@ -210,8 +235,13 @@
 		}
 	}
 
-	function maxThisMonthB() {
-		$sql = 'SELECT count(ip) AS count FROM cms_visit GROUP BY day HAVING MONTH(day) = '.date('m');
+	function maxThisMonthB($d = 0) {
+		$sql = 'SELECT count(ip) AS count FROM cms_visit GROUP BY day HAVING ';
+		if ($d == 0) {
+			$sql .='MONTH(day) = '.date('m');
+		} else {
+			$sql .= 'day > SUBDATE(NOW(), '.$d.')';
+		}
 		$result = mysql_query($sql);
 		if ($result) {
 			$maxVisitors = 0;
@@ -226,8 +256,13 @@
 		}
 	}
 
-	function maxThisMonthC() {
-		$sql = 'SELECT day, bots FROM cms_bots WHERE MONTH(day) = '.date('m');
+	function maxThisMonthC($d = 0) {
+		$sql = 'SELECT day, bots FROM cms_bots WHERE ';
+		if ($d == 0) {
+			$sql .='MONTH(day) = '.date('m');
+		} else {
+			$sql .= 'day > SUBDATE(NOW(), '.$d.')';
+		}
 		$result = mysql_query($sql);
 		if ($result) {
 			$maxVisitors = 0;
